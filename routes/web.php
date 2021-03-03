41cac83335947c5ca0b0a946e33fc2d49e70430b
haruo.ikeda <oruhadakei@gmail.com>
2021-03-03 06:46:42 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
+
+ // ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
